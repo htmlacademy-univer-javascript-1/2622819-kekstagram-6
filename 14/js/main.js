46ed9190fd124d6a66223c44e28b44eaee06a4db
Keks <keks@htmlacademy.ru>
2025-12-26 11:45:42 +0300

    :heavy_check_mark: Сборка #14

---
+++ 14/js/main.js
@@ -0,0 +1,29 @@
+import { renderPictures } from './pictures.js';
+import { loadPhotos } from './api.js';
+import './form.js';
+
+const showDataError = () => {
+  const error = document.createElement('div');
+  error.style.position = 'fixed';
+  error.style.top = '20px';
+  error.style.left = '50%';
+  error.style.transform = 'translateX(-50%)';
+  error.style.padding = '15px 25px';
+  error.style.backgroundColor = '#ff4d4d';
+  error.style.color = '#fff';
+  error.style.borderRadius = '5px';
+  error.style.zIndex = '1000';
+  error.textContent = 'Не удалось загрузить данные. Попробуйте обновить страницу';
+
+  document.body.appendChild(error);
+
+  setTimeout(() => {
+    error.remove();
+  }, 5000);
+};
+
+loadPhotos()
+  .then((photos) => {
+    renderPictures(photos);
+  })
+  .catch(showDataError);
